a2dccdfe26521d8518c89eaa4f4e132b74ae249d
JuanNandrissaR <juannandrissa@yahoo.com>
2021-01-10 14:47:09 +0700

    update timeline dan membuat php tweet

---
+++ createTweet.php
@@ -0,0 +1,20 @@
+<?php session_start();
+require_once('config.php');
+
+if(!isset($_POST['tweet'])){
+    exit;
+}
+
+$user_id = $_SESSION['AkunLogin'];
+$userData = $collection1->findOne(array('_id'=>$user_id));
+$tweet = substr($_POST['tweet'], 140);
+$date = date('Y-m-d H:i:s');
+
+$collection2->insertOne(array(
+    'authorId' => $user_id,
+    'authorName' => $userData['NamaPengguna'],
+    'tweet' => $tweet,
+    'created' => date
+));
+
+header('Location:timeline.php');
