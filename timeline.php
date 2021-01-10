043a977cca6005095fb95609159d6414c61d4e3a
JuanNandrissaR <juannandrissa@yahoo.com>
2021-01-10 14:03:32 +0700

    Update timeline

--- timeline.php
+++ timeline.php
@@ -86,7 +86,7 @@
                             <form action="" method="POST">
                                 <div class="form-group">
                                     <label for="Tweet">Tweet :</label>
-                                    <input type="text" name="Tweet" class="form-control" placeholder="compose new tweet...">
+                                    <textarea type="text" name="Tweet" class="form-control" required="" placeholder="Apa yang sedang terjadi?"></textarea>
                                 </div>
                                 <input type="submit" class="btn btn-primary" value="Tweet" name="posting">
                             </form>
